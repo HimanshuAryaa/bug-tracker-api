de05992e7e99c6bcae9b85a81465046b11fd6362
Himanshu Arya <49977125+HimanshuAryaa@users.noreply.github.com>
2026-05-27 11:44:36 +0530

    Updating changes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify, request
-from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
+from flask import Flask
+from flask_jwt_extended import JWTManager
 from extensions import db, bcrypt
 from models import User, Project, Bug
 from routes.auth import auth
@@ -21,13 +21,9 @@ app.register_blueprint(project)
 app.register_blueprint(bug)
 bcrypt.init_app(app)
 
-@app.route("/me", methods=["GET"])
-@jwt_required()
-def me():
-    from models import User
-    user_id = get_jwt_identity()
-    user = User.query.get(user_id)
-    return jsonify(user.to_dict())
+@app.route("/")
+def home():
+    return "Bug Tracker API is running!"
 
 with app.app_context():
     db.create_all()
