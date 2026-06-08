32377622c451c466552264f1e22ad64087388a61
Himanshu Arya <49977125+HimanshuAryaa@users.noreply.github.com>
2026-06-08 10:42:40 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -44,13 +44,12 @@ class Bug(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(300), nullable=False)
-    severity = db.Column(db.String(10), nullable=False)
-    priority = db.Column(db.String(10), nullable=False)
-    status = db.Column(db.String(10), nullable=False)
+    severity = db.Column(db.String(20), nullable=False)
+    priority = db.Column(db.String(20), nullable=False)
+    status = db.Column(db.String(20), nullable=False)
     project_id = db.Column(db.Integer, db.ForeignKey("project.id"), nullable=False)
     reporter_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     assignee_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    #created_at = db.Column(db.DateTime, default=datetime.utcnow)
     created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
 
     def to_dict(self):
